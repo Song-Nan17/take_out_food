285447604ae1ea5183ac8b7adecf7cefddae602d
Song <songnan17@qq.com>
2019-01-28 00:33:14 +0800

    Format Item.

--- src/Item.java
+++ src/Item.java
@@ -1,9 +1,10 @@
 public class Item {
-    public String id,name;
+    public String id, name;
     public double price;
-    public  Item(String id,String name, double price) {
-        this.id=id;
-        this.name=name;
-        this.price=price;
+
+    public Item(String id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
     }
 }
