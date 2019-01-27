7d6b3c4f6b02ae14ed90aac00954e19e8981cff4
Song <songnan17@qq.com>
2019-01-28 00:24:51 +0800

    Add Item & out.

---
+++ src/Item.java
@@ -0,0 +1,9 @@
+public class Item {
+    public String id,name;
+    public double price;
+    public  Item(String id,String name, double price) {
+        this.id=id;
+        this.name=name;
+        this.price=price;
+    }
+}
