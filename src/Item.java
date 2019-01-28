d1c22019e2c56f85ffbd84c67eeb1e700dec50cf
Song <songnan17@qq.com>
2019-01-28 17:39:35 +0800

    Modify Item.

--- src/Item.java
+++ src/Item.java
@@ -1,10 +1,22 @@
 public class Item {
-    public String id, name;
-    public double price;
+    private String id, name;
+    private double price;
 
     public Item(String id, String name, double price) {
         this.id = id;
         this.name = name;
         this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
     }
 }
